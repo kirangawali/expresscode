232f7e0add3508f59b7d2d12f1f6c9bd06d9d472
KIRAN <kirangawali09@yahoo.com>
2020-01-18 21:08:19 +0530

    basic

---
+++ express_coding/Basic/Basic-Express-Part-3/code/server.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+
+const firstApp = require("./routes/firstapp");
+
+app.use('/apps',firstApp);
+
+app.get('/', function(req, res){
+   res.send("Hello world!");
+});
+
+app.get('/first', function(req, res){
+   res.send("First Hello world!");
+});
+
+app.get('/second', function(req, res){
+   res.send("Second Hello world!");
+});
+
+app.listen(3000,function(){
+console.log('Express Server listening at port 3000...');
+}); 
